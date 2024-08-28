611ef242818ff9b2d714852a94bab0eec4ceadee
Allan <allan.delatuillerie@gmail.com>
2024-08-28 16:58:28 +0200

    Drop OK

--- Scripts/GridMap.cs
+++ Scripts/GridMap.cs
@@ -3,37 +3,47 @@ using System;
 
 public partial class GridMap : Control
 {
-	const int CELL_SIZE = 64;
-	Control gridMap;
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		gridMap = GetNode<Control>(".");
-	}
+    const int CELL_SIZE = 64;
+    Control gridMap;
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+    public override void _Ready()
+    {
+        gridMap = GetNode<Control>(".");
+    }
+
     public override bool _CanDropData(Vector2 atPosition, Variant data)
     {
-		return true;
+        // Calculer la position sur la grille
+        Vector2 gridPosition = (atPosition / CELL_SIZE).Floor() * CELL_SIZE;
+
+        // Vérifier si une pièce existe déjà à cette position
+        foreach (Node child in gridMap.GetChildren())
+        {
+            if (child is TextureRect textureRect)
+            {
+                if (textureRect.Position == gridPosition)
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
+
     public override void _DropData(Vector2 atPosition, Variant data)
     {
-		if (data.Obj is TextureRect textureRect)
-		{
-			Vector2 gridPosition = (atPosition / CELL_SIZE).Floor() * CELL_SIZE;
+        if (data.Obj is TextureRect textureRect)
+        {
+            Vector2 gridPosition = (atPosition / CELL_SIZE).Floor() * CELL_SIZE;
 
-			TextureRect newTextureRect = (TextureRect)textureRect.Duplicate();
-			newTextureRect.Position = gridPosition;
-
-			gridMap.AddChild(newTextureRect);
-		}
-		else
-		{
-			GD.Print("Le type de data n'est pas un TextureRect.");
-		}
-        base._DropData(atPosition, data);
+                TextureRect newTextureRect = (TextureRect)textureRect.Duplicate();
+                newTextureRect.Position = gridPosition;
+                
+                gridMap.AddChild(newTextureRect);
+        }
+        else
+        {
+            GD.Print("Le type de data n'est pas un TextureRect.");
+        }
     }
 }
