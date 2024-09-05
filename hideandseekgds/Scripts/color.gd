67363736ead3af85b2400ea4a5f85f01d3ba0509
Allan <allan.delatuillerie@gmail.com>
2024-09-05 14:28:39 +0200

    add score for each pieces

--- hideandseekgds/Scripts/color.gd
+++ hideandseekgds/Scripts/color.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @export var size = 0
+@export var score = 0
 @export var color_name = ""
 @onready var texture_rect: TextureRect = $TextureRect
 var is_vertical = true
--- hideandseekgds/Scripts/game.gd
+++ hideandseekgds/Scripts/game.gd
@@ -1,10 +1,18 @@
 extends Control
 
+@onready var grid_container: GridContainer = $GridContainer
+@onready var score_label: Label = $Label
+@onready var texture_rect_preview: TextureRect = $TextureRect
+@onready var restart_button: Button = $Restart
+@onready var change_piece_button: Button = $ChangePiece
+
 const GRID_SIZE = 10
 const BLANK_CELL = preload("res://Assets/blank.png") 
-@onready var texture_rect_preview: TextureRect = $TextureRect
+
+var score: int = 0
 var cells = []
 var selected_color = null 
+var attempt = 2
 var color_scenes = [
 	preload("res://Scenes/Yellow.tscn"),
 	preload("res://Scenes/Green.tscn"),
@@ -19,11 +27,12 @@ var color_previews = {
 }
 
 func _ready():
+	print(grid_container)
 	create_grid()
 	assign_random_color()
 
 func _process(delta: float) -> void:
-	rotate_color()
+	pass
 
 func create_grid():
 	for i in range(GRID_SIZE):
@@ -34,7 +43,7 @@ func create_grid():
 			cell_button.pressed.connect(func() -> void:
 				_button_pressed(i, j)
 			)
-			$GridContainer.add_child(cell_button)
+			grid_container.add_child(cell_button)
 			row.append(cell_button)
 		cells.append(row)
 
@@ -42,13 +51,8 @@ func _button_pressed(i: int, j: int) -> void:
 	var text_error = "ERROR"
 	if selected_color:
 		place_color(i, j)
-		# Vérifier s'il reste de la place pour la pièce actuelle
-		if not can_place_anywhere(selected_color):
+		if not check_grid(selected_color):
 			print("Aucune place disponible pour la pièce actuelle")
-			# Ajouter une action, comme afficher un popup ou un message ici
-		else:
-			# Générer une nouvelle couleur si la pièce actuelle ne peut plus être placée
-			assign_random_color()
 	else:
 		SignalBus.popup_displayed.emit(text_error)
 		#popup_container.add_child(popup_instance)
@@ -61,10 +65,12 @@ func place_color(i: int, j: int):
 			var x = i + n if selected_color.is_vertical else i
 			var y = j if selected_color.is_vertical else j + n
 			cells[x][y].icon = load("res://Assets/"+ selected_color.color_name +".png")
+		score_count()
 		assign_random_color()
 	else:
 		SignalBus.popup_displayed.emit(text_error)
 		#popup_container.add_child(popup_instance)
+		return
 
 func can_place_color(i: int, j: int, size: int, is_vertical: bool) -> bool:
 	for n in range(size):
@@ -76,20 +82,6 @@ func can_place_color(i: int, j: int, size: int, is_vertical: bool) -> bool:
 			return false
 	return true
 
-func rotate_color():
-	var text_error = "No color selected, you can't rotate."
-	if Input.is_action_just_released("rotate"):
-		if selected_color != null:
-			if selected_color.is_vertical == true:
-				selected_color.is_vertical = false
-				texture_rect_preview.rotation = -1.57079994678497
-			else:
-				selected_color.is_vertical = true
-				texture_rect_preview.rotation = 0
-	else:
-		SignalBus.popup_displayed.emit(text_error)
-		#popup_container.add_child(popup_instance)
-
 func assign_random_color():
 	texture_rect_preview.rotation = 0
 	var random_index = randi() % color_scenes.size()
@@ -98,7 +90,15 @@ func assign_random_color():
 	if color_previews.has(color_name):
 		texture_rect_preview.texture = color_previews[color_name]
 
-func can_place_anywhere(piece) -> bool:
+func check_grid(piece) -> bool:
+	var has_blank_cell = false
+	for row in cells:
+		for cell in row:
+			if cell.icon == BLANK_CELL:
+				has_blank_cell = true
+				break
+	if not has_blank_cell:
+		reset_grid()
 	for i in range(GRID_SIZE):
 		for j in range(GRID_SIZE):
 			if can_place_color(i, j, piece.size, true):
@@ -106,3 +106,45 @@ func can_place_anywhere(piece) -> bool:
 			if can_place_color(i, j, piece.size, false):
 				return true
 	return false
+
+func score_count():
+	score += selected_color.score
+	score_label.text = "Score: " + str(score)
+
+func reset_grid():
+	for row in cells:
+		for cell in row:
+			if cell.icon != BLANK_CELL:
+				cell.icon = load("res://Assets/blank.png")
+
+func _on_rotate_pressed() -> void:
+	if selected_color != null:
+		if selected_color.is_vertical == true:
+			selected_color.is_vertical = false
+			texture_rect_preview.rotation = -1.57079994678497
+		else:
+			selected_color.is_vertical = true
+			texture_rect_preview.rotation = 0
+
+func _on_restart_pressed() -> void:
+	reset_grid()
+	var enable_theme_button = load("res://Themes/Buttons.tres")
+	score_label.text = "Score: 0"
+	change_piece_button.text = "Change\nPiece\n1x"
+	attempt = 0
+	change_piece_button.set_theme(enable_theme_button)
+
+
+func _on_change_piece_pressed() -> void:
+	var disable_theme_button = load("res://Themes/Disable_Button.theme")
+	if attempt == 2:
+		assign_random_color()
+		change_piece_button.text = "Change\nPiece\n1x"
+		attempt = 1
+	elif attempt == 1:
+		assign_random_color()
+		change_piece_button.text = "No attempt!"
+		attempt = 0
+		change_piece_button.set_theme(disable_theme_button)
+	else:
+		pass
